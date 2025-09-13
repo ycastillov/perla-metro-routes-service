6d8c10ee1b6cbb4910738292367ad560be584f94
yamirCastilloV <166874609+yamirCastilloV@users.noreply.github.com>
2025-09-13 15:53:45 -0300

    feat: add .gitignore

--- obj/Debug/net9.0/PerlaMetro-RouteService.AssemblyInfo.cs
+++ obj/Debug/net9.0/PerlaMetro-RouteService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerlaMetro-RouteService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a3cc5649f8ae60e86c6aa76b77d35fdf893736a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerlaMetro-RouteService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerlaMetro-RouteService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
